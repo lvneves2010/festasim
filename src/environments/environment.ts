b804ad24db6b7d80c276c93f893e9dbe48f62e0a
Leonardo Neves <leonardo.neves@jadlog.com.br>
2026-05-02 17:32:23 -0300

    ajuste de versao 1_0_8

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5099/api',
-  appVersion: '1.0.7'
+  // apiUrl: 'http://localhost:5099/api',
+  appVersion: '1.0.8'
 };
 
 /*
